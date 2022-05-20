351e6f3224d2236dacb0ed7d470d3abff7437443
Rohit Tokala <rtokala@purdue.edu>
2022-05-20 15:00:27 +0400

    04 complete

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(inputArr) {
+    let others = Array.from(arguments);
+    for (let j = 1; j < arguments.length; j++) {
+        inputNum = others[j];
+        for (let i = 0; i < inputArr.length; i++) {
+            if (inputArr[i] === inputNum) {
+                inputArr.splice(i,1);
+                break;
+            }
+        }
+    }
+    return inputArr;
 };
 
 // Do not edit below this line

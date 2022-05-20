c88ff9ef791891be4a80dd28538f197521d4cd78
Rohit Tokala <rtokala@purdue.edu>
2022-05-20 14:46:01 +0400

    03 complete

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(inputStr) {
+    newArray = inputStr.split('');
+    newString = "";
+    for (let i = newArray.length-1; i >= 0; i--) {
+        newString += newArray[i];
+    }
+    return newString;
 };
 
 // Do not edit below this line

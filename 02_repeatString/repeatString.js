4775ac59bd7783202f61b7c5b4f1d8badd162d8e
Rohit Tokala <rtokala@purdue.edu>
2022-05-20 14:41:50 +0400

    02 complete

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(text, num) {
+    if (num < 0) {
+        return "ERROR";
+    }
+    output = "";
+    for (let i = 0; i < num; i++) {
+        output += text;
+    }
+    return output;
 };
 
 // Do not edit below this line

10c70ecd082a6576289eea3d1eb569cacc02a957
Rohit Tokala <rtokala@purdue.edu>
2022-05-23 17:34:47 +0400

    06 complete

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,10 @@
-const leapYears = function() {
-
+const leapYears = function(inputYear) {
+    if (inputYear % 100 === 0) {
+        if (inputYear % 400 === 0) return true;
+        else return false;
+    }
+    else if (inputYear % 4 === 0) return true;
+    else return false;
 };
 
 // Do not edit below this line

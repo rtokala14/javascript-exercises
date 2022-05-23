604e2a706cbde86256a159232122ba603514b934
Rohit Tokala <rtokala@purdue.edu>
2022-05-23 17:41:36 +0400

    07 complete

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(fTemp) {
+  let C = (fTemp - 32) * (5 / 9);
+  C = Math.round(C * 10) / 10;
+  return C;
 };
 
-const ctof = function() {
-
+const ctof = function(cTemp) {
+  let F = (cTemp * (9/5)) + 32;
+  F = Math.round(F * 10) / 10;
+  return F;
 };
 
 // Do not edit below this line

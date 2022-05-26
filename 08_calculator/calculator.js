fa9f6e8c798e11598a292d4dfbf355d0c4480b2b
Rohit Tokala <rtokala@purdue.edu>
2022-05-27 00:47:54 +0400

    complete

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
 const add = function() {
-	
+	return arguments[0] + arguments[1];
 };
 
 const subtract = function() {
-	
+	return arguments[0] - arguments[1];
 };
 
 const sum = function() {
-	
+	return arguments[0].reduce((total, num) => {
+    total += num;
+    return total;
+  }, 0)
 };
 
 const multiply = function() {
-
+  return arguments[0].reduce((prod, num) => {
+    prod *= num;
+    return prod;
+  }, 1);
 };
 
 const power = function() {
-	
+	return arguments[0] ** arguments[1];
 };
 
 const factorial = function() {
-	
+	const input = arguments[0];
+  if (input === 0) return 1;
+  let fact = 1;
+  for (let i = 1; i <= input; i++) {
+    fact *= i;
+  }
+  return fact;
 };
 
 // Do not edit below this line
